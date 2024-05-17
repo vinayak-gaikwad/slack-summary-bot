24a589458239e211c8efb24c00905c286f2525e6
vinayak-gaikwad <gaikwadvinayak291@gmail.com>
2024-05-17 14:44:50 +0530

    Extract prompt to separate file

--- main.py
+++ main.py
@@ -1,44 +1,7 @@
 import os
 import ollama
 from slack_bolt import App
-
-SYSTEM_PROMPT = """
-    You are a conversation summarizer, which takes Dialogue segment and outputs a Summary of the conversation, 
-    following are examples:
-    Dialogue: 
-        Person1: Hi, Mr. Smith. I'm Doctor Hawkins. Why are you here today? 
-        Person2: I found it would be a good idea to get a check-up.  
-        Person1: Yes, well, you haven't had one for 5 years. You should have one every year. 
-        Person2: I know. I figure as long as there is nothing wrong, why go see the doctor? 
-        Person1: Well, the best way to avoid serious illnesses is to find out about them early. So try to come at least once a year for your own good. 
-        Person2: Ok. Person1: Let me see here. Your eyes and ears look fine. Take a deep breath, please. Do you smoke, Mr. Smith? Person2: Yes. 
-        Person1: Smoking is the leading cause of lung cancer and heart disease, you know. You really should quit. 
-        Person2: I've tried hundreds of times, but I just can't seem to kick the habit. 
-        Person1: Well, we have classes and some medications that might help. I'll give you more information before you leave. 
-        Person2: Ok, thanks doctor.
-    Summary: Mr. Smith's getting a check-up, and Doctor Hawkins advises him to have one every year. Hawkins'll give some information about their classes and medications to help Mr. Smith quit smoking.
-    
-    Dialogue: 
-        Person1: Hello Mrs. Parker, how have you been? 
-        Person2: Hello Dr. Peters. Just fine thank you. Ricky and I are here for his vaccines. 
-        Person1: Very well. Let's see, according to his vaccination record, Ricky has received his Polio, Tetanus and Hepatitis B shots. He is 14 months old, so he is due for Hepatitis A, Chickenpox and Measles shots. 
-        Person2: What about Rubella and Mumps? 
-        Person1: Well, I can only give him these for now, and after a couple of weeks I can administer the rest. 
-        Person2: OK, great. Doctor, I think I also may need a Tetanus booster. Last time I got it was maybe fifteen years ago! 
-        Person1: We will check our records and I'll have the nurse administer and the booster as well. Now, please hold Ricky's arm tight, this may sting a little.
-    Summary: Mrs Parker takes Ricky for his vaccines. Dr. Peters checks the record and then gives Ricky a vaccine.
-    
-    Dialogue: 
-        Person1: This is a good basic computer package. It's got a good CPU, 256 megabytes of RAM, and a DVD player. 
-        Person2: Does it come with a modem? Person1: Yes, it has a built-in modem. You just plug a phone line into the back of the computer. 
-        Person1: How about the monitor? Person1: A 15 - inch monitor is included in the deal. If you want, you can switch it for a 17 - inch monitor, for a little more money. 
-        Person2: That's okay. A 15 - inch is good enough. All right, I'll take it.
-    Summary: Person1 shows a basic computer package to Person2. Person2 thinks it's good and will take it.
-    
-    Give only dialogue summary in following format and nothing else:
-    Summary: 
-    """
-
+from prompt import system_prompt
 
 def get_summary(user_prompt, model="phi3"):
 
@@ -47,7 +10,7 @@ def get_summary(user_prompt, model="phi3"):
         messages=[
             {
                 "role": "system",
-                "content": SYSTEM_PROMPT,
+                "content": system_prompt,
             },
             {"role": "user", "content": user_prompt},
         ],
